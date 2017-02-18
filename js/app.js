6d0bbb5af78b45cae9287e9a171ee1f6f8434eb5
Brandon Leichty <brandonl@me.com>
2017-02-17 21:22:39 -0600

    added form submission event handler

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
 'use strict';
+
+let userAlbumQuery;
+
+$('form').on('submit', (event) => {
+    event.preventDefault();
+    userAlbumQuery = $('#search').val().trim();
+    console.log(`Searching for ${userAlbumQuery}`);
+});
